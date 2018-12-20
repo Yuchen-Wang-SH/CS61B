14485a6507adbe868642644b46b81c1fd3b6a6c1
Yuchen Wang <yuchenwang.sh@gmail.com>
2018-12-20 17:36:04 +0800

    v1

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,61 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    private int N;
+    private PercolationFactory pf;
+
+    private double mean, std, lowCon, highCon;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        this.pf = pf;
+
+        double[] results = new double[T];
+        for (int i = 0; i < T; i++) {
+            results[i] = oneTrial();
+        }
+        mean = StdStats.mean(results);
+        std = StdStats.stddev(results);
+        lowCon = mean - 1.96 * std / Math.sqrt(T);
+        highCon = mean + 1.96 * std / Math.sqrt(T);
+    }
+
+    private double oneTrial() {
+        Percolation p = pf.make(N);
+
+        while (!p.percolates()) {
+            int[] site = randomSite();
+            p.open(site[0], site[1]);
+        }
+        return p.numberOfOpenSites() / (N * N);
+    }
+
+    private int[] randomSite() {
+        // site[0]: row, site[1]: col
+        int[] site = new int[2];
+        for (int i = 0; i < 2; i++) {
+            int index = StdRandom.uniform(N);
+            site[i] = index;
+        }
+        return site;
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return std;
+    }
+
+    public double confidenceLow() {
+        return lowCon;
+    }
+
+    public double confidenceHigh() {
+        return highCon;
+    }
 }
