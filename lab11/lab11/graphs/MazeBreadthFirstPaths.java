bc65ff3d6683cdb88b0d59562abc3ac6b680b3e4
Yuchen Wang <yuchenwang.sh@gmail.com>
2018-12-29 10:19:24 +0800

    Find Cycles using DFS.

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import sun.awt.image.ImageWatched;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +14,47 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> q = new LinkedList<>();
+        q.add(v);
+        marked[v] = true;
+        while(!q.isEmpty()) {
+            int vertex = q.remove();
+            for (int n: maze.adj(vertex)) {
+                if (!marked[n]) {
+                    marked[n] = true;
+                    edgeTo[n] = vertex;
+                    distTo[n] = distTo[vertex] + 1;
+                    announce();
+                    if (n == t) {
+                        return;
+                    }
+                    q.add(n);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs(s);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  *  @author Josh Hug
  */
@@ -9,14 +11,85 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private boolean found;
+    private int[] invisibleEdgeTo;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        found = false;
+        invisibleEdgeTo = new int[edgeTo.length];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        /** 1. Select a not-yet-visited vertex to start search. If all visited, return.
+         * 2. run dfs on this vertex to search for circle, if found, return.
+         * 3.if not found, goto 1.
+         */
+        while (true) {
+            int vToStart = findNotVisited(marked);
+            // If all vertices are visited, return -1, else return index.
+            if (vToStart == -1) {
+                return;
+            }
+            findCircle(vToStart, -1);
+            if (found) {
+                return;
+            }
+        }
+    }
+
+    private static int findNotVisited(boolean[] marked) {
+        /** Loop over marked. Return the index of the first element whose value
+         * is false. If all are true, return -1.
+         */
+        for (int i = 0; i < marked.length; i++) {
+            if (!marked[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void findCircle(int v, int parentV) {
+        /** Run dfs. If a neighbor is visited and it is not the parent of this current
+         * vertex, there is a circle. Print the circle and return true. Otherwise,
+         * return false.
+         */
+        marked[v] = true;
+        announce();
+        for (int n: maze.adj(v)) {
+            if (marked[n]) {
+                if (n != parentV) {
+                    found = true;
+                    invisibleEdgeTo[n] = v;
+                    drawCircle(n);
+                    return;
+                } else {
+                    continue;
+                }
+            }
+            invisibleEdgeTo[n] = v;
+            findCircle(n, v);
+            if (found) {
+                return;
+            }
+        }
+    }
+
+    private void drawCircle(int v) {
+        // Draw the circle which starts and ends at v.
+        int endPoint = v;
+        while (true) {
+            edgeTo[v] = invisibleEdgeTo[v];
+            v = edgeTo[v];
+            if (v == endPoint) {
+                announce();
+                return;
+            }
+        }
     }
 
     // Helper methods go here
